0c97f5f9b18aaa3935af74960ffc8919d0d82fa7
Zou Bibo <zoubibo@amiunique.net>
2023-01-07 13:20:26 +0800

    add long description

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="flitton_fib_py",
     version="0.0.1",
     author="Zou Bibo",
     author_email="zoubibo@amiunique.net",
     description="Calculates a Fibonacci number",
-    long_description="A basic library that \
-      calculates Fibonacci numbers",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/hekinami/flitton- \
       fib-py",
